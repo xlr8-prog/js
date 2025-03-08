bb7e6305a9331af2252a6fc9c52c4bf14409c37b
xlr8-prog <boulagdourzohair@gmail.com>
2025-03-08 20:52:15 +0000

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,38 @@
+
+        function commaSeparateNumber(val) {
+        while (/(\d+)(\d{3})/.test(val.toString())) {
+        val = val.toString().replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+        }
+        return val;
+        }
+
+        function countup() {
+        $('.stat_holder').each(function() {
+        var $this = $(this),
+        countTo = $this.attr('data-count');
+
+        $({
+        countNum: $this.text()
+        }).animate({
+        countNum: countTo
+        },
+
+        {
+        duration: 3000,
+        easing: 'linear',
+        step: function() {
+        $this.text(commaSeparateNumber(Math.floor(this.countNum)));
+        },
+        complete: function() {
+        $this.text(commaSeparateNumber(this.countNum));
+        //alert('finished');
+        }
+        }
+        );
+        });
+        }
+
+        $().ready(function(){
+            countup();
+        });
+
