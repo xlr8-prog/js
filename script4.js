4b56fc5e84acc2249720944ebc6b3723068d7b7f
xlr8-prog <boulagdourzohair@gmail.com>
2025-03-15 23:27:49 +0000

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,4 +1,4 @@
-<script>
+    // script 4
     // دالة للانتقال إلى الصفحة 1
     function goToPage1() {
         document.getElementById("homePage").style.display = "none";
@@ -62,4 +62,3 @@
     //     document.getElementById("page2").style.display = "none";
     // };
 
-</script>
